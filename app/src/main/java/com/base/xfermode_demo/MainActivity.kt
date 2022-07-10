bf0dce298ac2a0f239beb859e5b91af048061854
jiangshiyu <3434481891@qq.com>
2022-07-10 16:57:15 +0800

    Initial commit

---
+++ app/src/main/java/com/base/xfermode_demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.base.xfermode_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
